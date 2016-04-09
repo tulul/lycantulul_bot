cb6e87928bc72a43f40118f19fc46ccca75bf8bd
Rick Daniel <rick@araishikeiwai.com>
2016-04-09 15:13:47 +0100

    latest test

--- testscript.rb
+++ testscript.rb
@@ -24,7 +24,7 @@ def print_game
   puts "#{'seen'.ljust(10)}: #{@g.seen.map{ |a| "#{a[:seer_id]}, #{a[:full_name]}" } }"
   puts "#{'protectee'.ljust(10)}: #{@g.protectee.map{ |a| "#{a[:protector_id]}, #{a[:full_name]}" } }"
   puts "#{'votee'.ljust(10)}: #{@g.votee.map{ |a| "#{a[:voter_id]}, #{a[:full_name]}" } }"
-  @g.players.each do |a| 
+  @g.players.each do |a|
     puts "#{a.user_id} : #{a.role} : #{a.alive ? 1 : 0}"
   end
   puts
@@ -36,7 +36,7 @@ def event(message)
 end
 
 def start_vote
-  Lycantulul::DiscussionTimerJob.new.perform(@g)
+  LycantululBot.message_action(game, LycantululBot::VOTING_START)
 end
 
 event('reset db')
@@ -55,6 +55,12 @@ a6 = Telegram::Bot::Types::User.bikin(6)
 a7 = Telegram::Bot::Types::User.bikin(7)
 a8 = Telegram::Bot::Types::User.bikin(8)
 a9 = Telegram::Bot::Types::User.bikin(9)
+a10 = Telegram::Bot::Types::User.bikin(10)
+a11 = Telegram::Bot::Types::User.bikin(11)
+a12 = Telegram::Bot::Types::User.bikin(12)
+a13 = Telegram::Bot::Types::User.bikin(13)
+a14 = Telegram::Bot::Types::User.bikin(14)
+a15 = Telegram::Bot::Types::User.bikin(15)
 
 event('registration')
 a0.pm('/daftar')
@@ -67,32 +73,38 @@ a6.pm('/daftar')
 a7.pm('/daftar')
 a8.pm('/daftar')
 a9.pm('/daftar')
+a10.pm('/daftar')
+a11.pm('/daftar')
+a12.pm('/daftar')
+a13.pm('/daftar')
+a14.pm('/daftar')
+a15.pm('/daftar')
 
-event('invalid commands')
-a0.gr('/daftar')
-a0.gr('/batalin')
-a0.gr('/gajadi')
-a0.gr('/ikutan')
-a0.gr('/mulai_main')
-a0.gr('/siapa_aja')
-a0.gr('/panggil_semua')
-a0.gr('/panggil_yang_idup')
-a0.gr('/ilangin_keyboard')
+# event('invalid commands')
+# a0.gr('/daftar')
+# a0.gr('/batalin')
+# a0.gr('/gajadi')
+# a0.gr('/ikutan')
+# a0.gr('/mulai_main')
+# a0.gr('/siapa_aja')
+# a0.gr('/panggil_semua')
+# a0.gr('/panggil_yang_idup')
+# a0.gr('/ilangin_keyboard')
 
-event('cancel')
-a0.gr('/bikin_baru')
-a0.gr('/batalin')
+# event('cancel')
+# a0.gr('/bikin_baru')
+# a0.gr('/batalin')
 
-event('everyone leaves')
-a0.gr('/bikin_baru')
-a1.gr('/ikutan')
-a0.gr('/gajadi')
-a1.gr('/gajadi')
+# event('everyone leaves')
+# a0.gr('/bikin_baru')
+# a1.gr('/ikutan')
+# a0.gr('/gajadi')
+# a1.gr('/gajadi')
 
-event('not enough players')
-a0.gr('/bikin_baru')
-a1.gr('/ikutan')
-a1.gr('/mulai_main')
+# event('not enough players')
+# a0.gr('/bikin_baru')
+# a1.gr('/ikutan')
+# a1.gr('/mulai_main')
 
 event('start game')
 a0.gr('/bikin_baru')
@@ -105,19 +117,31 @@ a6.gr('/ikutan')
 a7.gr('/ikutan')
 a8.gr('/ikutan')
 a9.gr('/ikutan')
+a10.gr('/ikutan')
+a11.gr('/ikutan')
+a12.gr('/ikutan')
+a13.gr('/ikutan')
+a14.gr('/ikutan')
+a15.gr('/ikutan')
 a0.gr('/mulai_main')
 
-event('cant action to self')
-a0.pm('0')
-a1.pm('1')
-a2.pm('2')
-a3.pm('3')
-a4.pm('4')
-a5.pm('5')
-a6.pm('6')
-a7.pm('7')
-a8.pm('8')
-a9.pm('9')
+# event('cant action to self')
+# a0.pm('0')
+# a1.pm('1')
+# a2.pm('2')
+# a3.pm('3')
+# a4.pm('4')
+# a5.pm('5')
+# a6.pm('6')
+# a7.pm('7')
+# a8.pm('8')
+# a9.pm('9')
+# a10.pm('10')
+# a11.pm('11')
+# a12.pm('12')
+# a13.pm('13')
+# a14.pm('14')
+# a15.pm('15')
 
 event('modify game contents')
 @g = Lycantulul::Game.find_by(group_id: 21, finished: false, night: true, waiting: false)
@@ -125,104 +149,178 @@ event('modify game contents')
 @g.update_attribute(:waiting, false)
 @g.players.with_id(0).assign(Lycantulul::Game::WEREWOLF)
 @g.players.with_id(1).assign(Lycantulul::Game::WEREWOLF)
-@g.players.with_id(2).assign(Lycantulul::Game::SEER)
-@g.players.with_id(3).assign(Lycantulul::Game::PROTECTOR)
+@g.players.with_id(2).assign(Lycantulul::Game::WEREWOLF)
+@g.players.with_id(3).assign(Lycantulul::Game::SEER)
+@g.players.with_id(4).assign(Lycantulul::Game::SEER)
+@g.players.with_id(5).assign(Lycantulul::Game::PROTECTOR)
+@g.players.with_id(6).assign(Lycantulul::Game::NECROMANCER)
+@g.players.with_id(7).assign(Lycantulul::Game::SILVER_BULLET)
 print_game
 
-event('werewolves no majority vote')
-a0.pm('5')
-a1.pm('6')
-a2.pm('7')
-a3.pm('8')
-print_game
+# event('werewolves no majority vote')
+# a0.pm('5')
+# a1.pm('6')
+# a2.pm('7')
+# a3.pm('8')
+# print_game
 
-event('voting no majority vote')
-start_vote
-a0.pm('1')
-a1.pm('2')
-a2.pm('3')
-a3.pm('4')
-a4.pm('5')
-a5.pm('6')
-a6.pm('7')
-a7.pm('8')
-a8.pm('9')
-a9.pm('0')
-print_game
+# event('voting no majority vote')
+# start_vote
+# a0.pm('1')
+# a1.pm('2')
+# a2.pm('3')
+# a3.pm('4')
+# a4.pm('5')
+# a5.pm('6')
+# a6.pm('7')
+# a7.pm('8')
+# a8.pm('9')
+# a9.pm('0')
+# print_game
 
-event('werewolves kill successful')
-a0.pm('5')
-a1.pm('5')
-a2.pm('6')
-a3.pm('7')
-print_game
+# event('werewolves kill successful')
+# a0.pm('5')
+# a1.pm('5')
+# a2.pm('6')
+# a3.pm('7')
+# print_game
 
-event('execute innocent')
-start_vote
-a0.pm('6')
-a1.pm('6')
-a2.pm('6')
-a3.pm('6')
-a4.pm('6')
-a6.pm('3')
-a7.pm('6')
-a8.pm('6')
-a9.pm('6')
-print_game
+# event('execute innocent')
+# start_vote
+# a0.pm('6')
+# a1.pm('6')
+# a2.pm('6')
+# a3.pm('6')
+# a4.pm('6')
+# a6.pm('3')
+# a7.pm('6')
+# a8.pm('6')
+# a9.pm('6')
+# print_game
 
-event('under protection of jimat')
+# event('under protection of jimat')
+# a0.pm('7')
+# a1.pm('7')
+# a2.pm('7')
+# a3.pm('7')
+# print_game
+
+# event('execute werewolf')
+# start_vote
+# a0.pm('1')
+# a1.pm('0')
+# a2.pm('0')
+# a3.pm('0')
+# a4.pm('0')
+# a7.pm('0')
+# a8.pm('0')
+# a9.pm('0')
+# print_game
+
+# event('seen killed by werewolves')
+# a1.pm('7')
+# a2.pm('7')
+# a3.pm('2')
+# print_game
+
+# event('execute no one')
+# start_vote
+# a1.pm('8')
+# a2.pm('8')
+# a3.pm('8')
+# a4.pm('1')
+# a8.pm('1')
+# a9.pm('1')
+# print_game
+
+# event('seer killed by werewolves')
+# a1.pm('2')
+# a2.pm('9')
+# a3.pm('4')
+# print_game
+
+# event('execute innocent')
+# start_vote
+# a1.pm('9')
+# a3.pm('9')
+# a4.pm('9')
+# a8.pm('9')
+# a9.pm('8')
+# print_game
+
+# event('jimat protects werewolf, dies, werewolf wins')
+# a1.pm('8')
+# a3.pm('1')
+# print_game
+
+event('werewolves kill silver bullet')
 a0.pm('7')
 a1.pm('7')
 a2.pm('7')
-a3.pm('7')
-print_game
-
-event('execute werewolf')
-start_vote
-a0.pm('1')
-a1.pm('0')
-a2.pm('0')
-a3.pm('0')
-a4.pm('0')
-a7.pm('0')
-a8.pm('0')
-a9.pm('0')
-print_game
-
-event('seen killed by werewolves')
-a1.pm('7')
-a2.pm('7')
-a3.pm('2')
-print_game
-
-event('execute no one')
-start_vote
-a1.pm('8')
-a2.pm('8')
-a3.pm('8')
-a4.pm('1')
-a8.pm('1')
-a9.pm('1')
-print_game
-
-event('seer killed by werewolves')
-a1.pm('2')
-a2.pm('9')
-a3.pm('4')
+a3.pm('15')
+a4.pm('15')
+a5.pm('15')
+a6.pm(Lycantulul::Game::NECROMANCER_SKIP)
 print_game
 
 event('execute innocent')
-start_vote
-a1.pm('9')
-a3.pm('9')
-a4.pm('9')
+a0.pm('8')
+a1.pm('8')
+a2.pm('8')
+a3.pm('8')
+a4.pm('8')
+a5.pm('8')
+a6.pm('8')
+a7.pm('8')
 a8.pm('9')
 a9.pm('8')
+a10.pm('8')
+a11.pm('8')
+a12.pm('8')
+a13.pm('8')
+a14.pm('8')
+a15.pm('8')
+
+event('necromancer raises someone')
+@g.players.with_id(0).kill
+@g.players.with_id(1).kill
+@g.players.with_id(2).kill
+@g.players.with_id(3).kill
+@g.players.with_id(4).kill
+@g.players.with_id(5).kill
+a6.pm('1')
 print_game
 
-event('jimat protects werewolf, dies, werewolf wins')
-a1.pm('8')
-a3.pm('1')
+event('execute innocent')
+a0.pm('9')
+a1.pm('9')
+a2.pm('9')
+a3.pm('9')
+a4.pm('9')
+a5.pm('9')
+a6.pm('9')
+a7.pm('9')
+a8.pm('9')
+a9.pm('10')
+a10.pm('9')
+a11.pm('9')
+a12.pm('9')
+a13.pm('9')
+a14.pm('9')
+a15.pm('9')
+
+event('necromancer killed before raising')
+@g.players.with_id(0).revive
+@g.players.with_id(1).revive
+@g.players.with_id(2).revive
+@g.players.with_id(3).kill
+@g.players.with_id(4).kill
+@g.players.with_id(5).kill
+@g.players.with_id(6).revive
+a0.pm('6')
+a1.pm('6')
+a2.pm('6')
+a6.pm('9')
 print_game
 
 event('reset db')
