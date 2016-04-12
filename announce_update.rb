5b54bffbe6a17376da22f584adf5c2f7f76b85ac
Rick Daniel <rick@araishikeiwai.com>
2016-04-12 18:25:45 +0100

    customise role count #3

--- announce_update.rb
+++ announce_update.rb
@@ -1,6 +1,10 @@
 load 'Rakefile'
 
 updates = "latest updates:\n"
+updates += "- /ganti_settingan_peran buat ngubah jumlah peran semau kita (cuma buat tts, intip, jimat, mujahid, ebola)\n"
+updates += "- /batal_nyetting_peran jadi kalo mau jalanin yang /ganti_settingan_peran, itu ada step2-nya. kalo mau ulang dari awal, batalin dulu pake command ini\n"
+updates += "- /apus_settingan_peran buat ngapus semua settingan peran yang udah dibikin"
+updates += "- game ga bisa mulai kalo jumlah yang di-setting > jumlah pemain (misal setting 3 tts, 3 jimat tapi yang main cuma 5. plis)"
 groups = Lycantulul::Game.all.map(&:group_id).uniq
 Telegram::Bot::Client.run($token) do |bot|
   groups.each do |g|
