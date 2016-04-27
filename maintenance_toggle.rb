e075a660d917f9071a06c19c4a3ab0c316f9b073
Rick Daniel <rick@araishikeiwai.com>
2016-04-27 14:59:53 +0100

    30 secs maintenance wait

--- maintenance_toggle.rb
+++ maintenance_toggle.rb
@@ -6,8 +6,8 @@ $redis.set('lycantulul::maintenance_prevent', rp ^ 1)
 res = $redis.get('lycantulul::maintenance').to_i rescue 0
 
 while res == 0 && (count = Lycantulul::Game.running.count) > 0
-  puts "Still #{count} game(s) running, sleeping for 5 seconds"
-  sleep(5)
+  puts "Still #{count} game(s) running, sleeping for 30 seconds"
+  sleep(30)
 end
 
 puts "Maintenance mode toggling to #{res ^ 1 == 0 ? 'deactivated' : 'activated' }"
