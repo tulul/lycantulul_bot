b8591b962865e978cfa584865f9bee4b3dfb0c81
Rick Daniel <rick@araishikeiwai.com>
2016-04-27 13:33:42 +0100

    some statistics

--- stats.rb
+++ stats.rb
@@ -2,7 +2,11 @@ load 'Rakefile'
 
 puts "Current stats:"
 puts
-puts "Games played: #{Lycantulul::Game.count}"
+puts "Games started: #{Lycantulul::Game.count}"
+puts "Ranked games played: #{g = Lycantulul::Group.all.sum(&:game)}"
+puts "Werewolf victory: #{w = Lycantulul::Group.all.sum(&:werewolf_victory)} (#{"%.2f\%" % (w * 100.0 / g)})"
+puts "Villager victory: #{v = Lycantulul::Group.all.sum(&:village_victory)} (#{"%.2f\%" % (v * 100.0 / g)})"
+puts
 puts "Registered players: #{Lycantulul::RegisteredPlayer.count}"
 puts "Registered groups: #{Lycantulul::Group.count}"
 puts
