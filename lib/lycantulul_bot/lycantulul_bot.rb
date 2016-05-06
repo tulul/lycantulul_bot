f437b037422b1cab8bea2ad9a69f2c868aececd9
Rick Daniel <rick@araishikeiwai.com>
2016-05-06 10:40:46 +0100

    502

--- lib/lycantulul_bot/lycantulul_bot.rb
+++ lib/lycantulul_bot/lycantulul_bot.rb
@@ -18,6 +18,11 @@ class LycantululBot
       sleep(3)
     end
     retry unless e.message =~ /[400|403|409]/
+  rescue StandardError => e
+    puts Time.now.utc
+    puts e.message
+    puts e.backtrace.select{ |err| err =~ /tulul/ }.join(', ')
+    retry
   end
 
   def self.log(message)
