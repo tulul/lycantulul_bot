2033caa136ce586a18107586803bd7928e92a29c
Rick Daniel <rick@araishikeiwai.com>
2016-08-21 17:26:59 +0100

    new announcement

--- announce_update.rb
+++ announce_update.rb
@@ -4,12 +4,14 @@ updates = []
 updates << 'latest updates:'
 updates << '- Biduan dilemahin jadi cuma bisa 1x selamat dari serangan serigala. Soalnya lumayan banyak yang pake peran itu, imba'
 updates << ''
-updates << "- <a href='https://github.com/tulul/lycantulul_bot'>Klik sini kalo mau saran/lapor bug/dll (github)</a>"
+updates << "- <a href='https://telegram.me/lycantulul_board'>Klik sini untuk join channel berisi update dari Lycantulul</a>"
+updates << "- <a href='https://github.com/tulul/lycantulul_bot'>Klik sini kalo mau saran/lapor bug/kontribusi/dll (github)</a>"
 updates << "- <a href='https://storebot.me/bot/lycantulul_bot'>Klik sini kalo mau ngasih rating/review (storebot)</a>"
 updates << "- <a href='https://telegram.me/lycantulul'>Klik sini kalo grup kalian sepi dan pengen main bareng di grup publik</a>"
 updates = updates.join("\n")
 
 groups = Lycantulul::Game.all.map(&:group_id).uniq
+puts "TARGET: #{groups.count}"
 
 success = 0
 failure = 0
@@ -27,10 +29,13 @@ Telegram::Bot::Client.run($token) do |bot|
         r = Lycantulul::Group.find_by(group_id: g)
         r && r.destroy
       end
+    ensure
+      if (success + failure) % 50 == 0
+        puts "#{success}/#{failure}"
+      end
     end
   end
 end
 
-puts "TARGET: #{groups.count}"
 puts "OK: #{success}"
 puts "NO: #{failure}"
--- announce_update_all.rb
+++ announce_update_all.rb
@@ -1,46 +1,24 @@
 load 'Rakefile'
 
 updates = []
-updates << '<b>[Sayembara Tulul]</b>'
-updates << 'Hai semua~'
-updates << "Mimin Tulul butuh kreativitas kalian untuk membuat logo yang menarik untuk Lycantulul! (iya ini yang dipake sekarang nyomot dari Google \xF0\x9F\x98\x82)"
+updates << 'Halo~'
+updates << "Lama tak berjumpa, I've been busy with dissertation~"
+updates << 'Ditinggal berbulan-bulan tanpa pengembangan apapun, tak disangka masih banyak pemain aktif, bahkan bertambah!'
+updates << "Terhitung sejak 17 Mei 2016, ada #{Lycantulul::Group.where(:created_at.gte => DateTime.new(2016, 5, 17)).count} grup baru, #{Lycantulul::RegisteredPlayer.where(:created_at.gte => DateTime.new(2016, 5, 17)).count} pemain baru, dan #{Lycantulul::Game.where(:created_at.gte => DateTime.new(2016, 5, 17)).count} permainan yang dilakukan!"
 updates << ''
-updates << 'Berikut syarat dan ketentuan logo:'
-updates << '- Memiliki unsur serigala'
-updates << '- Memiliki unsur tulisan "Lycantulul"'
-updates << '- Bagus'
-updates << '- Persegi'
+updates << 'Coming soon: Logo Baru Lycantulul! (...setelah ditinggal hampir tiga bulan)'
 updates << ''
-updates << 'Berikut syarat dan ketentuan pengiriman:'
-updates << '- Logo dengan format <b>.png</b> di-attach pada email'
-updates << '- Kirim ke <b>lycantulul@araishikeiwai.com</b> sebelum <b>1 Juni 2016 18.00 WIB</b>'
-updates << '- Tuliskan subject "<b>[Logo] Nama Pembuat | @username_telegram</b>" (<i>tanpa tanda kutip, ganti Nama Pembuat dan @username_telegram dengan nama dan username sendiri</i>). Contoh: [Logo] Rick Daniel | @araishikeiwai'
+updates << "<b>Meanwhile, join channel berisi update dari Lycantulul di </b><a href='https://telegram.me/lycantulul_board'>sini</a><b> ya~ Biar ndak kasih info via PM/group lagi</b>"
 updates << ''
-updates << 'Berikut syarat dan ketentuan sayembara:'
-updates << '- Logo yang tidak memenuhi syarat-syarat logo dan pengiriman di atas tidak akan diikutsertakan'
-updates << '- Boleh mengirimkan lebih dari satu logo'
-updates << '- Logo terbaik akan dipilih oleh tim juri dan dipakai di semua hal yang berkaitan dengan Lycantulul'
-updates << '- Logo terpilih menjadi hak milik eksklusif Mimin Tulul'
-updates << '- Hak milik eksklusif dibeli dengan harga <b>Rp 200.000</b>'
-updates << '- Pembuat logo terpilih akan dihubungi Mimin Tulul untuk mengirimkan berkas raw (preferably .svg) dan pengurusan pembayaran'
-updates << ''
-updates << 'Tim Juri:'
-updates << '- Rick Daniel (@araishikeiwai)'
-updates << ''
-updates << 'Ayo ayo dikirim logonya! Ada hadiah menanti!'
-updates << '<i>Kemurahan?</i>'
-updates << "YHA ini game-nya nirlaba, mana developer-nya hanya seorang mahasiswa, tinggalnya di ibu kota negara yang semua mahal-mahal harganya \xF0\x9F\x98\xA2"
-updates << ''
-updates << 'Ditunggu ya~'
-updates << "~Mimin Tulul \xF0\x9F\x92\x99"
-updates << ''
-updates << "- <a href='https://github.com/tulul/lycantulul_bot'>Klik sini kalo mau saran/lapor bug/dll (github)</a>"
+updates << "- <a href='https://telegram.me/lycantulul_board'>Klik sini untuk join channel berisi update dari Lycantulul</a>"
+updates << "- <a href='https://github.com/tulul/lycantulul_bot'>Klik sini kalo mau saran/lapor bug/kontribusi/dll (github)</a>"
 updates << "- <a href='https://storebot.me/bot/lycantulul_bot'>Klik sini kalo mau ngasih rating/review (storebot)</a>"
 updates << "- <a href='https://telegram.me/lycantulul'>Klik sini kalo grup kalian sepi dan pengen main bareng di grup publik</a>"
 updates = updates.join("\n")
 puts updates.length
 
 groups = Lycantulul::RegisteredPlayer.all.map(&:user_id).uniq
+puts "TARGET: #{groups.count}"
 
 success = 0
 failure = 0
@@ -57,10 +35,13 @@ Telegram::Bot::Client.run($token) do |bot|
       end
       failure += 1
       puts e.message
+    ensure
+      if (success + failure) % 50 == 0
+        puts "#{success}/#{failure}"
+      end
     end
   end
 end
 
-puts "TARGET: #{groups.count}"
 puts "OK: #{success}"
 puts "NO: #{failure}"
